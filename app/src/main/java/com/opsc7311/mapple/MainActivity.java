c8322fb4205e151775a4956a83c12d39f7db2e2b
Dimcon <daimonsewell@gmail.com>
2021-05-24 18:40:12 +0200

    Added test 4

--- app/src/main/java/com/opsc7311/mapple/MainActivity.java
+++ app/src/main/java/com/opsc7311/mapple/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         System.out.println("Testing");
+        System.out.println("Testing 4 ");
         setContentView(R.layout.activity_main);
     }
 }
