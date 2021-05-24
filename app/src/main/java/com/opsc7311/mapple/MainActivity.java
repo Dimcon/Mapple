a0147c6c9fa8acf4b24f83854f0a57a4de704568
Dimcon <daimonsewell@gmail.com>
2021-05-24 18:32:28 +0200

    Initial commit

---
+++ app/src/main/java/com/opsc7311/mapple/MainActivity.java
@@ -0,0 +1,14 @@
+package com.opsc7311.mapple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
